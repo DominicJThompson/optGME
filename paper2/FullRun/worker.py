c2a92970efcda65aa80da104f20fbd53afe0eac8
DominicJThompson <dominicjt23@gmail.com>
2025-12-09 13:54:08 -0500

    bugs

--- paper2/FullRun/worker.py
+++ paper2/FullRun/worker.py
@@ -2,15 +2,15 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--loss_index", type=int)
-parser.add_argument("--ndbp_index", type=int)
-parser.add_argument("--ngs_index", type=int)
-parser.add_argument("--seed", type=int)
+parser.add_argument("--LOSS_INDEX", type=int)
+parser.add_argument("--NDBP_INDEX", type=int)
+parser.add_argument("--NG_INDEX", type=int)
+parser.add_argument("--SEED", type=int)
 args = parser.parse_args()
 
-loss_index = args.loss_index
-ndbp_index = args.ndbp_index
-ngs_index = args.ngs_index
-seed = args.seed
+loss_index = args.LOSS_INDEX
+ndbp_index = args.NDBP_INDEX
+ngs_index = args.NG_INDEX
+seed = args.SEED
 
 print(f"Running with loss index {loss_index}, ndbp index {ndbp_index}, ngs index {ngs_index}, and seed {seed}")
